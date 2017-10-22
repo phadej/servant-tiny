b378de2ec27405d855c0a37534375b8ca4fd72f8
Oleg Grenrus <oleg.grenrus@iki.fi>
2017-10-22 20:36:03 +0300

    Experiment: combinators on the right of Verb

--- TinyServant.hs
+++ TinyServant.hs
@@ -9,12 +9,46 @@
 module TinyServant where
 
 import Control.Applicative
+import Control.Monad (join)
 import Data.Time
 import GHC.TypeLits
 import Text.Read
+import Data.Proxy (Proxy (..))
 
+-------------------------------------------------------------------------------
 -- API specification DSL
-data Get (a :: *)
+-------------------------------------------------------------------------------
+
+-- $apidsl
+--
+-- The idea is that
+--
+-- @
+-- a :> b :> Verb (c :< d)
+-- @
+--
+-- is transformed into
+--
+-- @
+-- a -> b -> m (c, (d, x))
+-- @
+--
+-- where (not so obviously) not only @->@ or @(,)@ may be applied.
+--
+-- So loosely:
+--
+-- * @:>@ is transformed into @->@,
+--
+-- * @:+@ is tranformed into product @(,)@ (or sum!),
+--
+-- * @Verb@ is a point where LHS and RHS meet, i.e. some monad @m@.
+
+data Verb (a :: *)
+data Full (a :: *)
+-- I'm not keen of this name, for streaming response it will be simply Stream;
+-- but what are non-stream response?
+--
+-- Also NoContent can be own response type.
 
 data a :<|> b = a :<|> b
 infixr 8 :<|>
@@ -22,64 +56,171 @@ infixr 8 :<|>
 data (a :: k) :> (b :: *)
 infixr 9 :>
 
+-- Q: Should we just reuse :> ?
+data (a :: l) :< (b :: *)
+infixr 9 :<
+
 data Capture (a :: *)
 
+-------------------------------------------------------------------------------
 -- Example API
-type MyAPI = "date" :> Get Day
-        :<|> "time" :> Capture TimeZone :> Get ZonedTime
+-------------------------------------------------------------------------------
 
-data Proxy a = Proxy
+-- | Day is always known, but for some timezones we might not know the time!
+--
+-- The key difference is that we declare which endpoints can fail.
+-- 'ServantErr' here is "unchecked exception", but it's possible to write
+-- checked exception variant.
+--
+type MyAPI = "date" :> Verb (Full Day)
+        :<|> "time" :> Capture TimeZone :> Verb (ServantErr :< Full ZonedTime)
 
--- The Server type family
-type family Server layout :: *
-type instance Server (Get a) = IO a
-type instance Server (a :<|> b) = Server a :<|> Server b
-type instance Server ((s :: Symbol) :> r) = Server r
-type instance Server (Capture a :> r) = a -> Server r
+-- | Type of errors.
+newtype ServantErr = ServantErr String
 
--- Handler for the example API
+-------------------------------------------------------------------------------
+-- Handlers
+-------------------------------------------------------------------------------
+
 handleDate :: IO Day
 handleDate = utctDay <$> getCurrentTime
 
-handleTime :: TimeZone -> IO ZonedTime
-handleTime tz = utcToZonedTime tz <$> getCurrentTime
+handleTime :: TimeZone -> IO (Either ServantErr ZonedTime)
+handleTime tz
+    | timeZoneMinutes tz < 0 = pure $ Left $ ServantErr
+        "We don't support timezones with negative offsets"
+    | otherwise             = Right . utcToZonedTime tz <$> getCurrentTime
 
 handleMyAPI :: Server MyAPI
 handleMyAPI = handleDate :<|> handleTime
 
--- The HasServer class
+-------------------------------------------------------------------------------
+-- Server + HasServer
+-------------------------------------------------------------------------------
+
+-- | The 'HasServer' class
 class HasServer layout where
-  route :: Proxy layout -> Server layout -> [String] -> Maybe (IO String)
+  type Server layout :: *
+  route :: Proxy layout -> Server layout -> [String] -> Either String (IO String)
+
+instance (HasServer a, HasServer b) => HasServer (a :<|> b) where
+  type Server (a :<|> b) = Server a :<|> Server b
+
+  route :: Proxy (a :<|> b) -> (Server a :<|> Server b) -> [String] -> Either String (IO String)
+  route _ (handlera :<|> handlerb) xs = pickRight
+    (route (Proxy :: Proxy a) handlera xs)
+    (route (Proxy :: Proxy b) handlerb xs)
+    where
+      pickRight (Right x) _ = Right x
+      pickRight (Left _)  y = y
+
+instance (KnownSymbol s, HasServer r) => HasServer (s :> r) where
+  type Server (s :> r) = Server r
+
+  route :: Proxy (s :> r) -> Server r -> [String] -> Either String (IO String)
+  route _ handler (x : xs)
+    | symbolVal (Proxy :: Proxy s) == x = route (Proxy :: Proxy r) handler xs
+  route _ _       _                     = Left "no match"
+
+instance (Read a, HasServer r) => HasServer (Capture a :> r) where
+  type Server (Capture a :> r) = a -> Server r
+
+  route :: Proxy (Capture a :> r) -> (a -> Server r) -> [String] -> Either String (IO String)
+  route _ handler (x : xs) = do
+    a <- maybe (Left "no parse") Right $ readMaybe x
+    route (Proxy :: Proxy r) (handler a) xs
+  route _ _       _        = Left "no match"
+
+instance (HasServer' a) => HasServer (Verb a) where
+  type Server (Verb a) = IO (Server' a)
+
+  route :: Proxy (Verb a) -> IO (Server' a) -> [String] -> Either String (IO String)
+  route _ handler [] = Right $ handler >>= route' (Proxy :: Proxy a)
+  route _ _       _  = Left "no match"
+
+class HasServer' a where
+  type Server' a :: *
+  route' :: Proxy a -> Server' a -> IO String
+
+instance HasServer' a => HasServer' (ServantErr :< a) where
+  type Server' (ServantErr :< a) = Either ServantErr (Server' a)
+
+  route' :: Proxy (ServantErr :< a) -> Either ServantErr (Server' a) -> IO String
+  route' _ (Left (ServantErr err)) = pure $ "HANDLER-ERROR: " ++ err
+  route' _ (Right x)        = route' (Proxy :: Proxy a) x
+
+instance Show a => HasServer' (Full a) where
+  type Server' (Full a) = a
+
+  route' :: Proxy (Full a) -> a -> IO String
+  route' _ x = pure (show x)
+
+  -- verb :: Show a => Proxy a -> Proxy b -> IO (ServerTrans b a) -> Either String (IO String)
+
+-------------------------------------------------------------------------------
+-- Serve
+-------------------------------------------------------------------------------
 
 serve :: HasServer layout
       => Proxy layout -> Server layout -> [String] -> IO String
 serve p h xs = case route p h xs of
-  Nothing -> ioError (userError "404")
-  Just m  -> m
+  Left err -> fail $ "ERROR: " ++ err
+  Right x  -> x
 
--- The HasServer instance
-type instance Server (Get a) = IO a
+{-
 
-instance Show a => HasServer (Get a) where
-  route :: Proxy (Get a) -> IO a -> [String] -> Maybe (IO String)
-  route _ handler [] = Just (show <$> handler)
-  route _ _       _  = Nothing
+From blog post: Works as previously:
 
-instance (HasServer a, HasServer b) => HasServer (a :<|> b) where
-  route :: Proxy (a :<|> b) -> (Server a :<|> Server b) -> [String] -> Maybe (IO String)
-  route _ (handlera :<|> handlerb) xs =
-        route (Proxy :: Proxy a) handlera xs
-    <|> route (Proxy :: Proxy b) handlerb xs
+*TinyServant> serve (Proxy :: Proxy MyAPI) handleMyAPI ["time", "CET"]
+"2017-10-22 17:14:59.382719319 CET"
+*TinyServant> serve (Proxy :: Proxy MyAPI) handleMyAPI ["time", "12"]
+*** Exception: user error (ERROR: no parse)
+*TinyServant> serve (Proxy :: Proxy MyAPI) handleMyAPI ["date"]
+"2017-10-22"
+*TinyServant> serve (Proxy :: Proxy MyAPI) handleMyAPI []
+*** Exception: user error (ERROR: no match)
 
-instance (KnownSymbol s, HasServer r) => HasServer ((s :: Symbol) :> r) where
-  route :: Proxy (s :> r) -> Server r -> [String] -> Maybe (IO String)
-  route _ handler (x : xs)
-    | symbolVal (Proxy :: Proxy s) == x = route (Proxy :: Proxy r) handler xs
-  route _ _       _                     = Nothing
+*TinyServant> serve (Proxy :: Proxy MyAPI) handleMyAPI ["time", "-01:00"]
+"HANDLER-ERROR: We don't support timezones with negative offsets"
+-}
 
-instance (Read a, HasServer r) => HasServer (Capture a :> r) where
-  route :: Proxy (Capture a :> r) -> (a -> Server r) -> [String] -> Maybe (IO String)
-  route _ handler (x : xs) = do
-    a <- readMaybe x
-    route (Proxy :: Proxy r) (handler a) xs
-  route _ _       _        = Nothing
+-------------------------------------------------------------------------------
+-- Bonus: No overlapping headers
+-------------------------------------------------------------------------------
+
+data Header (name :: Symbol) b
+
+instance (KnownSymbol name, Show b, HasServer' a) => HasServer' (Header name b :< a) where
+  type Server' (Header name b :< a) = (b, Server' a)
+
+  route' :: Proxy (Header name b :< a) -> (b, Server' a) -> IO String
+  route' _ (value, x) = do
+      x' <- route' (Proxy :: Proxy a) x
+      pure $ symbolVal (Proxy :: Proxy name) ++ ": " ++ show value ++ "; " ++ x'
+
+type HeaderAPI =
+  "time" :> Capture TimeZone :> Verb (Header "X-Tz" TimeZone :< ServantErr :< Full ZonedTime)
+
+-- |
+--
+-- @
+-- *TinyServant> :kind! Server HeaderAPI
+-- Server HeaderAPI :: *
+-- = TimeZone -> IO (String, Either ServantErr ZonedTime)
+-- @
+--
+-- Compare with 'handleTime'
+handleHeaderAPI :: Server HeaderAPI
+handleHeaderAPI tz
+    | timeZoneMinutes tz < 0 = pure
+        (tz, Left $ ServantErr "We don't support timezones with negative offsets")
+    | otherwise             = do
+        t <- getCurrentTime
+        pure (tz, Right (utcToZonedTime tz t))
+
+{-
+
+*TinyServant> serve (Proxy :: Proxy HeaderAPI) handleHeaderAPI  ["time", "-01:00"]
+"X-Tz: -0100; HANDLER-ERROR: We don't support timezones with negative offsets"
+
+-}
